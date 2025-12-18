a925cf905c741b66703175b9cceefa9c42cc5420
Lapraniteon <lapraniteon@gmail.com>
2025-12-18 10:18:37 +0100

    Update timelines

---
+++ Assets/UnityEventTrigger.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventTrigger : MonoBehaviour
+{
+    [System.Serializable]
+    public class TriggerEvent : UnityEvent<Collider> { }
+
+    public TriggerEvent OnTriggerEnterEvent;
+    public TriggerEvent OnTriggerExitEvent;
+
+    [Tooltip("If true, the trigger will activate on collision.")]
+    public bool activateOnCollision = true;
+
+    private bool isPlayerInside = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activateOnCollision) return;
+
+        if (other.GetComponent<CharacterController>() != null && !isPlayerInside)
+        {
+            OnTriggerEnterEvent.Invoke(other);
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!activateOnCollision) return;
+
+        if (other.GetComponent<CharacterController>() != null && isPlayerInside)
+        {
+            OnTriggerExitEvent.Invoke(other);
+            isPlayerInside = false;
+        }
+    }
+
+}
